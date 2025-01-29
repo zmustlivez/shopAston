40f99eddb9e06ab1c92230a0b7d9a931bdc1a787
Sentsova Maria <sentsovamaria@gmail.com>
2025-01-29 12:06:20 +0300

    Реализация методов для работы с таблицами Продукты

--- src/main/java/config/JDBCConnectionConfig.java
+++ src/main/java/config/JDBCConnectionConfig.java
@@ -5,20 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class Config {
+public class JDBCConnectionConfig {
 
     private static String URL;
     private static String USERNAME;
     private static String PASSWORD;
 
 
-    static Connection getJDBCConnection() {
+    public static Connection getJDBCConnection() {
         Connection connection = null;
         try {
             Properties properties = new Properties();
-            URL = properties.getProperty("spring.datasource.urlM");
-            USERNAME = properties.getProperty("spring.datasource.usernameM");
-            PASSWORD = properties.getProperty("spring.datasource.passwordM");
+            /*URL = properties.getProperty("spring.datasource.url");
+            USERNAME = properties.getProperty("spring.datasource.username");
+            PASSWORD = properties.getProperty("spring.datasource.password");*/
+            URL = "jdbc:postgresql://localhost:5432/postgres";
+            USERNAME = "postgres";
+            PASSWORD = "password";
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             System.out.println("Connection failed");
